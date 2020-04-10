1a51e796e9e0035a9cf80e4b07bd30eb1da1c606
Wenjie Zhou <wenjie.zhou@amlogic.com>
2020-04-10 01:33:06 -0800

    Revert "audio: new api to interface with hifidsp"
    This reverts commit c782b91b4684a8d18545abf93b4afb9e40b3d776.
    
    Change-Id: I9e828bd200d76a69fae3ccc5bbf9c7403280a3a2

--- hifi4rpc_test/hifi4rpc_pcm_test.c
+++ hifi4rpc_test/hifi4rpc_pcm_test.c
@@ -143,6 +143,142 @@ int pcm_play_test(int argc, char* argv[])
     return 0;
 }
 
+long get_us() {
+    struct timespec tp;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tp);
+    long long us =  tp.tv_sec * 1000 * 1000 + tp.tv_nsec / (1000);
+    // printf("cur us=%lld\n", us);
+    return us;
+}
+
+int bcm_file_test(int argc, char* argv[])
+{
+    printf("all arg begin\n");
+    int i;
+    for (i = 0; i != argc; i++) {
+        printf("arg[%d]=%s\n", i, argv[i]);
+    }
+    printf("all arg end\n");
+    int hdl = xAudio_Ipc_init();
+    AML_MEM_HANDLE hShmBuf;
+
+    FILE *fileplay = fopen("/data/out_lb.wav.raw", "rb");
+    if (fileplay == NULL) {
+        printf("failed to open played pcm file\n");
+        return -1;
+    }
+    const int ms = 16;
+    const int oneshot = 48 * ms; // 48KHz
+    uint32_t size = oneshot * 16 * 4; // 16channel, 32bit
+    uint32_t r;
+    hShmBuf = AML_MEM_Allocate(size);
+    void *buf = AML_MEM_GetVirtAddr(hShmBuf);
+    void *phybuf = AML_MEM_GetPhyAddr(hShmBuf);
+    int loop = 128 * 10; // test 10.24s
+    long d0, d1, e;
+    for (i = 0; i != loop; i++) {
+        d0 = get_us();
+        r = fread(buf, 1, size, fileplay);
+        if (r != size) {
+            rewind(fileplay);
+            r = fread(buf, 1, size, fileplay);
+        }
+        AML_MEM_Clean(phybuf, r);
+        bcm_client_write(hdl, phybuf, r);
+        d1 = get_us();
+        e = 16 * 1000 - (d1 - d0);
+        // if (i % (128 * 4) == 0) {
+        //     printf("real=%ld wait=%ld\n", d1 - d0, e);
+        // }
+        if (e > 0) {
+            usleep(e);
+        }
+        //printf("%dms pcm_write pcm=%p buf=%p in_fr=%d -> fr=%d xxx\n",
+        //   ms, p, buf, oneshot, fr);
+    }
+    AML_MEM_Free(hShmBuf);
+    fclose(fileplay);
+    xAudio_Ipc_Deinit(hdl);
+    return 0;
+}
+
+// !!! depend on libtinyalsa, only link in Android.mk !!!
+int bcm_pcm_test(int argc, char* argv[])
+{
+    int i;
+    for (i = 0; i != argc; i++) {
+        printf("arg[%d]=%s\n", i, argv[i]);
+    }
+#if 1
+    int hdl = xAudio_Ipc_init();
+    AML_MEM_HANDLE hShmBuf;
+
+    struct pcm_config cfg;
+    memset(&cfg, 0, sizeof(cfg));
+    cfg.channels = 16;
+    cfg.rate = 48000;
+    cfg.period_size = 128;
+    cfg.period_count = 4;
+    // !!! linux's TINYALSA side's header file's, PCM_FORMAT_S32_LE is 1
+    // !!! DSP's TINYALSA side's headefile, PCM_FORMAT_S32_LE is 7
+    cfg.format = 1;
+    cfg.start_threshold = 0;
+    cfg.stop_threshold = 0;
+    cfg.silence_threshold = 0;
+    uint32_t card = 0, device = 8, flags = PCM_IN;
+    printf("ch=%d rate=%d period=%dx%d format=%d thr=%d,%d,%d card=%d device=%d\n",
+           cfg.channels, cfg.rate, cfg.period_size, cfg.period_count,
+           cfg.format, cfg.start_threshold, cfg.stop_threshold,
+           cfg.silence_threshold, card, device);
+    struct pcm *pcm = pcm_open(card, device, flags, &cfg);
+    if (pcm == NULL) {
+        printf("failed to open pcm\n");
+        return -1;
+    }
+    printf("pcm=%p\n", pcm);
+    // if (!pcm_is_ready(pcm)) {
+    //     printf("pcm isn't ready\n");
+    //     return -2;
+    // }
+    const int ms = 16;
+    const int oneshot = 48 * ms; // 48KHz
+    uint32_t size = oneshot * 16 * 4; // 16channel, 32bit
+    uint32_t r;
+    hShmBuf = AML_MEM_Allocate(size);
+    void *buf = AML_MEM_GetVirtAddr(hShmBuf);
+    void *phybuf = AML_MEM_GetPhyAddr(hShmBuf);
+    int loop = 128 * 10;
+    for (i = 0; i != loop; i++) {
+        /** Linux tinyalsa's pcm_read, it return error code
+         * When r=0, it means successfully get full filled buffer */
+        r = pcm_read(pcm, buf, size);
+        if (r != 0) {
+            printf("expect size=%u, but get r=%u, quit\n", size, r);
+            break;
+        }
+        // int32_t *sp = (char *)buf;
+        // printf("%08x %08x %08x %08x\n", sp[0], sp[1], sp[2], sp[3]);
+        AML_MEM_Clean(phybuf, size);
+        bcm_client_write(hdl, phybuf, size);
+        //printf("%dms pcm_write pcm=%p buf=%p in_fr=%d -> fr=%d xxx\n",
+        //   ms, p, buf, oneshot, fr);
+    }
+    printf("i=%d loop=%d\n", i, loop);
+    AML_MEM_Free(hShmBuf);
+    pcm_close(pcm);
+    xAudio_Ipc_Deinit(hdl);
+#endif
+    return 0;
+}
+
+#include "aipc_type.h"
+int xaf_test(int argc, char **argv) {
+    int h = xAudio_Ipc_init();
+    xAIPC(h, MBX_CMD_XAF_TEST, NULL, 0);
+    xAudio_Ipc_Deinit(h);
+    return 0;
+}
+
 #define PCM_CAPTURE_SAMPLES (48000*20)
 int pcm_capture_test(int argc, char* argv[])
 {
